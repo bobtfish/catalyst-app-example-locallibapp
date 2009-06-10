e2078df8bed4285011dfc5d1ce84c4c25f1da1c2
t0m <bobtfish@bobtfish.net>
2009-06-11 00:28:21 +0100

    wget and unwrapping tarballs can die in a fire, just depend on local::lib

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-export PERL5LIB=
-wget http://search.cpan.org/CPAN/authors/id/A/AP/APEIRON/local-lib-1.004001.tar.gz
-tar xzf local-lib-1.004001.tar.gz
-cd local-lib-1.004001
-
-perl Makefile.PL --bootstrap=../local_lib
-make test
-make install
-cd ..
-rm -rf local-lib-1.004001* 
-
-export PERL5LIB=
-eval $(perl -Ilocal_lib/lib/perl5 -Mlocal::lib=--self-contained,local_lib)
-
 perl -MCPAN -Mlocal::lib=--self-contained,local_lib -e'force(qw/install local::lib/)'
+export PERL5LIB=
+perl -MCPAN -Mlocal::lib=--self-contained,local_lib -e'force(qw/install local::lib/)'
+exec script/installdeps.sh
 
