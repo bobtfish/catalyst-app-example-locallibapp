b855dd08a9779da456aa47632b2c9e47444b8951
John Napiorkowski <jjn1056@yahoo.com>
2009-07-21 21:43:11 -0400

    removed unneeded scripts

--- script/cpan-shell.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-exec /usr/bin/env perl -e 'use FindBin; BEGIN {do "$FindBin::Script/env" or die "no $FindBin::Script/env, err:".$@ } use CPAN; use local::lib qw(--self-contained local_lib); shell';
-
